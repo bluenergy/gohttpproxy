dc34b9c21470ce6134c197c1f507b611819361f7
cnmade <hufeng1987@gmail.com>
2023-01-19 17:16:35 +0800

    更新超时时间

--- martian/ioplus/ioplus.go
+++ martian/ioplus/ioplus.go
@@ -57,7 +57,7 @@ func InnerCopyBuffer(dst io.Writer, src io.Reader, buf []byte, fn func()) (writt
 		if nr > 0 {
 			//执行回调
 			if fn != nil {
-				//go fn()
+				go fn()
 			}
 			nw, ew := dst.Write(buf[0:nr])
 			if nw < 0 || nr < nw {
