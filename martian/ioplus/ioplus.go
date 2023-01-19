26eeac75b30271136bfb0f8059726a138e3e13bf
cnmade <hufeng1987@gmail.com>
2023-01-19 17:04:18 +0800

    优化iocopy

--- martian/ioplus/ioplus.go
+++ martian/ioplus/ioplus.go
@@ -1,55 +1,9 @@
 package ioplus
 
-import "errors"
-
-var ErrShortWrite = errors.New("short write")
-
-var errInvalidWrite = errors.New("invalid write result")
-
-// Reader is the interface that wraps the basic Read method.
-//
-// Read reads up to len(p) bytes into p. It returns the number of bytes
-// read (0 <= n <= len(p)) and any error encountered. Even if Read
-// returns n < len(p), it may use all of p as scratch space during the call.
-// If some data is available but not len(p) bytes, Read conventionally
-// returns what is available instead of waiting for more.
-//
-// When Read encounters an error or end-of-file condition after
-// successfully reading n > 0 bytes, it returns the number of
-// bytes read. It may return the (non-nil) error from the same call
-// or return the error (and n == 0) from a subsequent call.
-// An instance of this general case is that a Reader returning
-// a non-zero number of bytes at the end of the input stream may
-// return either err == EOF or err == nil. The next Read should
-// return 0, EOF.
-//
-// Callers should always process the n > 0 bytes returned before
-// considering the error err. Doing so correctly handles I/O errors
-// that happen after reading some bytes and also both of the
-// allowed EOF behaviors.
-//
-// Implementations of Read are discouraged from returning a
-// zero byte count with a nil error, except when len(p) == 0.
-// Callers should treat a return of 0 and nil as indicating that
-// nothing happened; in particular it does not indicate EOF.
-//
-// Implementations must not retain p.
-type Reader interface {
-	Read(p []byte) (n int, err error)
-}
-
-// Writer is the interface that wraps the basic Write method.
-//
-// Write writes len(p) bytes from p to the underlying data stream.
-// It returns the number of bytes written from p (0 <= n <= len(p))
-// and any error encountered that caused the write to stop early.
-// Write must return a non-nil error if it returns n < len(p).
-// Write must not modify the slice data, even temporarily.
-//
-// Implementations must not retain p.
-type Writer interface {
-	Write(p []byte) (n int, err error)
-}
+import (
+	"errors"
+	"io"
+)
 
 // Copy copies from src to dst until either EOF is reached
 // on src or an error occurs. It returns the number of bytes
@@ -63,89 +17,33 @@ type Writer interface {
 // the copy is implemented by calling src.WriteTo(dst).
 // Otherwise, if dst implements the ReaderFrom interface,
 // the copy is implemented by calling dst.ReadFrom(src).
-func Copy(dst Writer, src Reader, fn func()) (written int64, err error) {
+func Copy(dst io.Writer, src io.Reader, fn func()) (written int64, err error) {
 	return InnerCopyBuffer(dst, src, nil, fn)
 }
-
-// CopyBuffer is identical to Copy except that it stages through the
-// provided buffer (if one is required) rather than allocating a
-// temporary one. If buf is nil, one is allocated; otherwise if it has
-// zero length, CopyBuffer panics.
-//
-// If either src implements WriterTo or dst implements ReaderFrom,
-// buf will not be used to perform the copy.
-func CopyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
-	if buf != nil && len(buf) == 0 {
-		panic("empty buffer in CopyBuffer")
-	}
-	return InnerCopyBuffer(dst, src, buf, nil)
-}
-
-// ReaderFrom is the interface that wraps the ReadFrom method.
-//
-// ReadFrom reads data from r until EOF or error.
-// The return value n is the number of bytes read.
-// Any error except EOF encountered during the read is also returned.
-//
-// The Copy function uses ReaderFrom if available.
-type ReaderFrom interface {
-	ReadFrom(r Reader) (n int64, err error)
-}
-
-// WriterTo is the interface that wraps the WriteTo method.
-//
-// WriteTo writes data to w until there's no more data to write or
-// when an error occurs. The return value n is the number of bytes
-// written. Any error encountered during the write is also returned.
-//
-// The Copy function uses WriterTo if available.
-type WriterTo interface {
-	WriteTo(w Writer) (n int64, err error)
-}
-
-var EOF = errors.New("EOF")
 
 // LimitReader returns a Reader that reads from r
 // but stops with EOF after n bytes.
 // The underlying implementation is a *LimitedReader.
-func LimitReader(r Reader, n int64) Reader { return &LimitedReader{r, n} }
-
 // A LimitedReader reads from R but limits the amount of
 // data returned to just N bytes. Each call to Read
 // updates N to reflect the new amount remaining.
 // Read returns EOF when N <= 0 or when the underlying R returns EOF.
-type LimitedReader struct {
-	R Reader // underlying reader
-	N int64  // max bytes remaining
-}
-
-func (l *LimitedReader) Read(p []byte) (n int, err error) {
-	if l.N <= 0 {
-		return 0, EOF
-	}
-	if int64(len(p)) > l.N {
-		p = p[0:l.N]
-	}
-	n, err = l.R.Read(p)
-	l.N -= int64(n)
-	return
-}
 
 // copyBuffer is the actual implementation of Copy and CopyBuffer.
 // if buf is nil, one is allocated.
-func InnerCopyBuffer(dst Writer, src Reader, buf []byte, fn func()) (written int64, err error) {
+func InnerCopyBuffer(dst io.Writer, src io.Reader, buf []byte, fn func()) (written int64, err error) {
 	// If the reader has a WriteTo method, use it to do the copy.
 	// Avoids an allocation and a copy.
-	if wt, ok := src.(WriterTo); ok {
+	if wt, ok := src.(io.WriterTo); ok {
 		return wt.WriteTo(dst)
 	}
 	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
-	if rt, ok := dst.(ReaderFrom); ok {
+	if rt, ok := dst.(io.ReaderFrom); ok {
 		return rt.ReadFrom(src)
 	}
 	if buf == nil {
 		size := 32 * 1024
-		if l, ok := src.(*LimitedReader); ok && int64(size) > l.N {
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
 			if l.N < 1 {
 				size = 1
 			} else {
@@ -159,12 +57,13 @@ func InnerCopyBuffer(dst Writer, src Reader, buf []byte, fn func()) (written int
 		if nr > 0 {
 			//执行回调
 			if fn != nil {
-				go fn()
+				//go fn()
 			}
 			nw, ew := dst.Write(buf[0:nr])
 			if nw < 0 || nr < nw {
 				nw = 0
 				if ew == nil {
+					errInvalidWrite := errors.New("invalid write")
 					ew = errInvalidWrite
 				}
 			}
@@ -174,12 +73,12 @@ func InnerCopyBuffer(dst Writer, src Reader, buf []byte, fn func()) (written int
 				break
 			}
 			if nr != nw {
-				err = ErrShortWrite
+				err = io.ErrShortWrite
 				break
 			}
 		}
 		if er != nil {
-			if er != EOF {
+			if er != io.EOF {
 				err = er
 			}
 			break
