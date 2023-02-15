5c5af031878b1bdd297edfbc5106ab47e70827bc
xfdf <root@localhost>
2023-02-15 22:32:26 +0800

    do not need update

--- martian/idleconn/idleconn.go
+++ martian/idleconn/idleconn.go
@@ -2,10 +2,14 @@ package idleconn
 
 import (
 	"net"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type IdleTimeoutConnV3 struct {
+	sync.Mutex
+	atomic.Int64
 	update func()
 	Conn   net.Conn
 }
@@ -24,9 +28,18 @@ func (ic *IdleTimeoutConnV3) Read(buf []byte) (int, error) {
 }
 
 func (ic *IdleTimeoutConnV3) UpdateIdleTime() {
-	_ = ic.Conn.SetReadDeadline(time.Now().Add(45 * time.Second))
-	_ = ic.Conn.SetWriteDeadline(time.Now().Add(45 * time.Second))
-	ic.update()
+	if ic.TryLock() {
+		defer ic.Unlock()
+
+		tmpInt := ic.Load()
+		if tmpInt <= 0 || tmpInt < time.Now().UnixMilli() {
+			ic.Store(time.Now().Add(6 * time.Second).UnixMilli())
+			//	log.Infof(" yes , do update because : %v", tmpInt)
+			_ = ic.Conn.SetReadDeadline(time.Now().Add(45 * time.Second))
+			_ = ic.Conn.SetWriteDeadline(time.Now().Add(45 * time.Second))
+			ic.update()
+		}
+	}
 }
 
 func (ic *IdleTimeoutConnV3) Write(buf []byte) (int, error) {
