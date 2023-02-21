85b724d590f0dc7fedc10ab8da7467e536ea6935
cnmade <hufeng1987@gmail.com>
2023-02-21 16:59:23 +0800

    Update idleConn

--- martian/idleconn/idleconn.go
+++ martian/idleconn/idleconn.go
@@ -1,50 +1,53 @@
 package idleconn
 
 import (
+	"github.com/gohttpproxy/gohttpproxy/martian/log"
 	"net"
 )
 
 type IdleTimeoutConnV3 struct {
 	update  func()
 	Conn    net.Conn
-	Updated chan int
+	Updated chan bool
 }
 
 func NewIdleTimeoutConnV3(conn net.Conn, fn func()) *IdleTimeoutConnV3 {
 	c := &IdleTimeoutConnV3{
 		Conn:    conn,
 		update:  fn,
-		Updated: make(chan int),
+		Updated: make(chan bool),
 	}
+	log.Infof(" create NewIdleTimeoutConnV3")
 	return c
 }
 
 func (ic *IdleTimeoutConnV3) Read(buf []byte) (int, error) {
-	select {
-	case ic.Updated <- 1:
-	default:
 
-	}
 	go ic.UpdateIdleTime()
+	select {
+	case ic.Updated <- true:
+	default:
+	}
 	return ic.Conn.Read(buf)
 }
 
 func (ic *IdleTimeoutConnV3) UpdateIdleTime() {
+
 	select {
 	case <-ic.Updated:
 		go ic.update()
 	default:
-
 	}
 }
 
 func (ic *IdleTimeoutConnV3) Write(buf []byte) (int, error) {
+
+	go ic.UpdateIdleTime()
 	select {
-	case ic.Updated <- 1:
+	case ic.Updated <- true:
 	default:
 
 	}
-	go ic.UpdateIdleTime()
 	return ic.Conn.Write(buf)
 }
 
